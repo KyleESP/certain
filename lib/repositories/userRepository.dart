0a3ac9c3a2a03a2bf0f7dc3352b6785403e7b440
Kyle <espkyle@gmail.com>
2020-09-28 09:58:07 +0200

    #1 - Création model et repository de User

---
+++ lib/repositories/userRepository.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class UserRepository {
+  final FirebaseAuth _firebaseAuth;
+  final FirebaseFirestore _firebaseFirestore;
+
+  UserRepository({FirebaseAuth firebaseAuth, FirebaseFirestore firestore})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        _firebaseFirestore = firestore ?? FirebaseFirestore.instance;
+
+  Future<void> signUpWithEmailAndPassword(String email, String password) async {
+    print(_firebaseAuth);
+    return await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<bool> isFirstTime(String userId) async {
+    bool userExists;
+    await _firebaseFirestore.collection('users').doc(userId).get().then((user) {
+      userExists = user.exists;
+    });
+
+    return userExists;
+  }
+
+  Future<void> signInWithEmail(String email, String password) {
+    return _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<bool> isSignedIn() async {
+    return _firebaseAuth.currentUser != null;
+  }
+
+  Future<void> signOut() async {
+    return await _firebaseAuth.signOut();
+  }
+
+  Future<String> getUser() async {
+    return _firebaseAuth.currentUser.uid;
+  }
+
+  Future<void> createProfile(
+      File photo,
+      String userId,
+      String name,
+      String gender,
+      String interestedIn,
+      DateTime age,
+      GeoPoint location) async {
+    StorageUploadTask storageUploadTask;
+    storageUploadTask = FirebaseStorage.instance
+        .ref()
+        .child('userPhotos')
+        .child(userId)
+        .child(userId)
+        .putFile(photo);
+
+    return await storageUploadTask.onComplete.then((ref) async {
+      await ref.ref.getDownloadURL().then((url) async {
+        await _firebaseFirestore.collection('users').doc(userId).set({
+          'uid': userId,
+          'photoUrl': url,
+          'name': name,
+          'location': location,
+          'gender': gender,
+          'interestedIn': interestedIn,
+          'age': age
+        });
+      });
+    });
+  }
+}
