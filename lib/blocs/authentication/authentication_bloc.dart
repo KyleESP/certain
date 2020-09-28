7ce917ea18ffb04cb9fcb887f35cd8173df9d40d
Kyle <espkyle@gmail.com>
2020-09-28 10:11:15 +0200

    #1 - Création bloc Authentication + MyBlocObserver

---
+++ lib/blocs/authentication/authentication_bloc.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'bloc.dart';
+
+import 'package:bloc/bloc.dart';
+
+import 'package:certain/repositories/userRepository.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository _userRepository;
+
+  AuthenticationBloc(this._userRepository) : super(Uninitialized());
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AppStarted) {
+      yield* _mapAppStartedToState();
+    } else if (event is LoggedIn) {
+      yield* _mapLoggedInToState();
+    } else if (event is LoggedOut) {
+      yield* _mapLoggedOutToState();
+    }
+  }
+
+  Stream<AuthenticationState> _mapAppStartedToState() async* {
+    try {
+      final isSignedIn = await _userRepository.isSignedIn();
+      if (isSignedIn) {
+        final uid = await _userRepository.getUser();
+        final isFirstTime = await _userRepository.userExists(uid);
+
+        if (!isFirstTime) {
+          yield AuthenticatedButNotSet(uid);
+        } else {
+          yield Authenticated(uid);
+        }
+      } else {
+        yield Unauthenticated();
+      }
+    } catch (_) {
+      yield Unauthenticated();
+    }
+  }
+
+  Stream<AuthenticationState> _mapLoggedInToState() async* {
+    final userExists =
+        await _userRepository.userExists(await _userRepository.getUser());
+
+    if (!userExists) {
+      yield AuthenticatedButNotSet(await _userRepository.getUser());
+    } else {
+      yield Authenticated(await _userRepository.getUser());
+    }
+  }
+
+  Stream<AuthenticationState> _mapLoggedOutToState() async* {
+    yield Unauthenticated();
+    _userRepository.signOut();
+  }
+}
